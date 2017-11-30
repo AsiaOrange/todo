356070bbcfbd6a52acf753cd2b204d5c1ce1a19d
Asia <ling379@qq.com>
2017-11-30 08:42:28 +0800

    basic

--- src/TodoList/index.jsx
+++ src/TodoList/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const TodoList = ({todos,handleAddList,handleDone }) => {
+const TodoList = ({todos,handleAddList,handleDone,handleEditButton,handleMinList }) => {
 
 	const todolists = todos.map((todo) => 
 		<li key={todo.todone.toString()}>
@@ -9,7 +9,11 @@ const TodoList = ({todos,handleAddList,handleDone }) => {
 			  checked={todo.isDone} 
 	  		  onChange={() => handleDone(todo)}
 	  		/>
-			<span>{todo.todone}</span>
+			{todo.isEdit ? <input type="text" value={todo.todone} 
+				/> : <span>{todo.todone}</span>}
+			{todo.isEdit ? <button>Save</button> : <button onClick={() => handleEditButton(todo)}>Edited</button>}
+			{todo.isEdit ? <button>Cancel</button> : <button onClick={() => handleMinList(todo)}>删除</button>}
+
 		</li>
 	);
 						
@@ -39,17 +43,18 @@ class TodoContainer extends React.Component {
 			todos: [
 				{
 					todone: "ss",
-					isDone:false
+					isDone:false,
+					isEdit:false
 				},
 				{
 					todone:"ax",
-					isDone:false
+					isDone:false,
+					isEdit:false
 				},
 			],
 		};
 	}
-// because do not know state 什么时候更新了  所以有个prevState 但是似乎不能做修改 官方给出的例子是获取 
-//所以先把this.state的值获取到
+// because do not know state 什么时候更新了  所以有个prevState or 先获取
 
 // 添加任务
 	handleAddList = (e) => {
@@ -65,12 +70,13 @@ class TodoContainer extends React.Component {
 			}
 			
 			
-				todos = todos.concat([{todone:sdo,isDone:false}]);
-				//use push 报错“map is not a function” try to use concat success
-				console.log({todos});
-				this.setState({todos});
-				console.log({todos});
-				e.target.value = ""; 
+			todos = todos.concat([{todone:sdo,isDone:false,isEdit:false}]);
+			//不能使用push因为他直接作用于原数组但state是immutable
+			//slice,concat,filter会返回新数组
+			console.log({todos});
+			this.setState({todos});
+			console.log({todos});
+			e.target.value = ""; 
 			
 		}
 	}
@@ -81,22 +87,59 @@ class TodoContainer extends React.Component {
 		for(let todo of todos){
 			if(edited.todone === todo.todone){
 				todo.isDone = !todo.isDone;
-				this.setState({todos:todos});
 				break;
 			}
 		}
+		this.setState({todos:todos});
 		
 	}
+
+
+//删减任务
+    handleMinList = (minus) => {
+    	alert("Delete");
+    	console.log({minus});
+    	//先使用变量获取了state值也可以直接在this.setState()使用prevState
+    	let todos = this.state.todos;
+    	console.log({todos});
+    	
+    	todos = todos.filter((todo) => {
+    		return minus.todone !== todo.todone;
+    	});	//previous no return  当删除其中一个时会全部删除 why?
+
+    	console.log({todos});	
+    	
+    	this.setState({todos});
+    }
+
+
+//EditButton
+    handleEditButton = (edit,e) =>{
+    	console.log("ss");
+    	let todos = this.state.todos;
+    	for(let todo of todos){
+    		if(todo.todone === edit.todone){
+    			todo.isEdit = true;
+    			break;
+    		}
+    	}
+    	this.setState({todos:todos});
+    }
+
+//EditInput
+
 
 
 
 	render(){
 		return(
 			<TodoList
-				 todos={this.state.todos}
-				 handleAddList={this.handleAddList} 
-				 handleDone={this.handleDone}
-
+				todos={this.state.todos}
+				handleAddList={this.handleAddList} 
+				handleDone={this.handleDone}
+				handleMinList={this.handleMinList}
+				handleEditButton={this.handleEditButton}
+				
 			 />
 		);
 	}
